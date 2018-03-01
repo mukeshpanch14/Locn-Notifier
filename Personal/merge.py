f7c57634587fa6432ef52fea18955c697b4708aa
mukeshpanch14 <mukesh.panch14@gmail.com>
2018-03-01 12:31:17 +0000

    1

--- Personal/merge.py
+++ Personal/merge.py
@@ -8,8 +8,8 @@ path4=open('bills.pdf','rb')
 merger = PyPDF2.PdfFileMerger()
  
 merger.merge(position=0, fileobj=path)
-merger.merge(position=2, fileobj=path2)
+merger.merge(position=1, fileobj=path2)
 merger.merge(position=3, fileobj=path3)
-merger.merge(position=4, fileobj=path4)
+merger.merge(position=10, fileobj=path4)
 
 merger.write(open("test_out.pdf", 'wb'))
