25d7cd4c092c12823f8e1064002a375abfd80669
mukeshpanch14 <mukesh.panch14@gmail.com>
2018-03-01 10:30:15 +0000

    1

---
+++ Personal/test.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+with open("newpdf.pdf",'rb') as infile:
+    reader=PdfFileReader(infile)
+    writer=PdfFileWriter()
+    writer.addPage(reader.getPage(0))
+
+with open('output.pdf','wb') as outfile:
+    writer.write(outfile)
